e8568fe20d43abaf2567d25aa7b207ca418311a7
Manish Patel <mdp3030@gmail.com>
2019-08-16 11:36:12 +0530

    - Trying bouncer animation

--- app/src/main/java/com/kotlindemo/activity/otherthings/ui/AutoTextViewDemoActivity.kt
+++ app/src/main/java/com/kotlindemo/activity/otherthings/ui/AutoTextViewDemoActivity.kt
@@ -5,9 +5,10 @@ import com.kotlindemo.R
 import com.kotlindemo.application.ParentActivity
 import kotlinx.android.synthetic.main.activity_auto_textview.*
 import kotlinx.android.synthetic.main.activity_auto_textview.toolbar
-import android.view.animation.BounceInterpolator
-import android.R.attr.button
-import android.animation.ObjectAnimator
+import android.view.animation.Animation
+import com.kotlindemo.utility.MyBounceInterpolator
+import android.view.animation.AnimationUtils
+
 
 /**
  * Created by Manish Patel on 8/10/2019.
@@ -25,32 +26,25 @@ class AutoTextViewDemoActivity : ParentActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Load the animation
-        /*val myAnim = AnimationUtils.loadAnimation(this,R.anim.bounce)
-        val animationDuration = 3 * 1000
-        myAnim.duration = animationDuration.toLong()
+        val myAnim = AnimationUtils.loadAnimation(this,R.anim.bounce_2)
+        //val animationDuration = 3 * 1000
+        //myAnim.duration = animationDuration.toLong()
 
         // Use custom animation interpolator to achieve the bounce effect
-        val interpolator = MyBounceInterpolator(0.2, 20.0)
+        /*val interpolator = MyBounceInterpolator(0.2, 20.0)
         myAnim.interpolator = interpolator*/
 
-        val animY = ObjectAnimator.ofFloat(button, "translationY", -100f, 0f)
-        animY.duration = 1000//1sec
-        animY.interpolator = BounceInterpolator()
-        animY.repeatCount = 1
-        animY.start()
-
         bounceButton.setOnClickListener {
-            animY.start()
-            //bounceButton.startAnimation(myAnim)
+            bounceButton.startAnimation(myAnim)
         }
 
         // Run button animation again after it finished
-        /*myAnim.setAnimationListener(object : Animation.AnimationListener {
+        myAnim.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(arg0: Animation) {}
             override fun onAnimationRepeat(arg0: Animation) {}
             override fun onAnimationEnd(arg0: Animation) {
                 bounceButton.clearAnimation()
             }
-        })*/
+        })
     }
 }
